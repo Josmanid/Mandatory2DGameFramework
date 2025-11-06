62539dd5aa028c8400443613e351d9f2f81b3943
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-11-06 15:39:28 +0100

    Lavet et loot bart objekt

---
+++ Mandatory2DGameFramework/model/Loot/LootChest.cs
@@ -0,0 +1,31 @@
+﻿using Mandatory2DGameFramework.worlds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.model.Loot
+{
+    public class LootChest : WorldObject
+    {
+        public List<WorldObject> Items { get; private set; }
+
+        public LootChest() {
+            Name = "Loot Chest";
+            Lootable = true;
+            Removeable = true;
+            Items = new List<WorldObject>();
+        }
+
+        public void AddItem(WorldObject item) {
+            Items.Add(item);
+        }
+
+        public override string ToString() {
+            return $"{Name} containing {Items.Count} items.";
+        }
+
+
+    }
+}
