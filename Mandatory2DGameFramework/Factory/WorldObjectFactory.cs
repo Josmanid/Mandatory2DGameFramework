8dd9970ccaaf8a983f3dd3f0dcd0e53398cb9a57
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-11-12 15:54:31 +0100

    Factory method lavet så den kan lave mine objecter for mig!

---
+++ Mandatory2DGameFramework/Factory/WorldObjectFactory.cs
@@ -0,0 +1,26 @@
+﻿using Mandatory2DGameFramework.model.attack;
+using Mandatory2DGameFramework.model.defence;
+using Mandatory2DGameFramework.worlds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.Factory
+{
+    public static class WorldObjectFactory
+    {
+        public static WorldObject Create(string type, int x, int y, string name, int value, int range = 0) {
+            switch (type.ToLower())
+            {
+                case "attack":
+                    return new AttackItem(x, y, name, hit: value, range: range);
+                case "defence":
+                    return new DefenceItem(x, y, name, reduce: value);
+                default:
+                    throw new ArgumentNullException($"Unkown world object: {type} do not exist ");
+            }
+        }
+    }
+}
