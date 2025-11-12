21526a2203d3961cf83ce5e8aa88eb0c77e9d48e
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-11-12 16:18:14 +0100

    Weak Goblin made for slaying

---
+++ Mandatory2DGameFramework/model/Creatures/Goblin.cs
@@ -0,0 +1,20 @@
+﻿using Mandatory2DGameFramework.Logging;
+using Mandatory2DGameFramework.model.attack;
+using Mandatory2DGameFramework.model.defence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.model.Creatures
+{
+    public class Goblin : Creature
+    {
+        MyLogger logger = MyLogger.Instance;
+        public Goblin() {
+            Name = "Weak Goblin";
+            HitPoint = 30;
+        }
+    }
+}
